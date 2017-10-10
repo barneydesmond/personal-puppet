3f2e138de60398b2c3f2d68f139c2c9d62098af0
Barney Desmond <barneydesmond@gmail.com>
2017-10-10 23:47:04 +1100

    Add root rc files

--- modules/soe/manifests/users.pp
+++ modules/soe/manifests/users.pp
@@ -2,4 +2,20 @@
 class soe::users {
   $users = hiera('soe::users')
   soe::user { $users: }
+
+  # root is special and is always handled
+  file {
+    '/root/.bashrc':
+      source => 'puppet:///modules/soe/root/bashrc';
+    '/root/.screenrc':
+      source => 'puppet:///modules/soe/root/screenrc';
+    '/root/.vimrc':
+      source => 'puppet:///modules/soe/root/vimrc';
+    '/root/.toprc':
+      source => 'puppet:///modules/soe/root/toprc';
+    '/root/.gitconfig':
+      source => 'puppet:///modules/soe/root/gitconfig';
+    '/root/.gitignore':
+      source => 'puppet:///modules/soe/root/gitignore';
+  }
 }
