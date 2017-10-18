a2f8162d0f595ad9122290d1182f72ac99477859
Barney Desmond <barneydesmond@gmail.com>
2017-10-18 07:28:57 +0000

    Add SSL Makefile

--- modules/soe/manifests/init.pp
+++ modules/soe/manifests/init.pp
@@ -3,5 +3,10 @@ class soe {
   class { 'soe::packages': }
   class { 'soe::users': }
 
+  file {
+    '/etc/ssl/Makefile':
+      source => 'puppet:///modules/soe/etc/ssl/Makefile';
+  }
+
   # Do sshd later
 }
